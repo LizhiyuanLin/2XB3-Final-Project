7b4d4d9d0feb1de330453b0fd59861bcce651bbd
Doris Yu <yux26@mcmaster.ca>
2019-04-13 23:19:23 -0400

    test structure

---
+++ 2XB3-Final-Project/src/temp/IndustryTest.java
@@ -0,0 +1,29 @@
+package temp;
+
+import static org.junit.Assert.*;
+
+/*
+ * Test cases for Assignment and Textbook reference code are omitted for simplicity.
+ */
+
+import org.junit.Test;
+
+public class IndustryTest {
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+	
+	public void testGraphRecommendation(){
+		fail("Not yet implemented");
+	}
+	
+	public void testRelaventIndustries() {
+		fail("Not yet implemented");
+	}
+	
+	public void testIndustry() {
+		fail("Not yet implemented");
+	}
+}
