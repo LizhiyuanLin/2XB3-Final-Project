45b073c331d8af57111ec88a20a5605ca7aa45ce
Mengtong_Shi <shim17@mcmaster.ca>
2019-04-14 11:06:16 -0400

    Added test cases

--- 2XB3-Final-Project/src/temp/IndustryTest.java
+++ 2XB3-Final-Project/src/temp/IndustryTest.java
@@ -2,28 +2,123 @@ package temp;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /*
  * Test cases for Assignment and Textbook reference code are omitted for simplicity.
  */
 
-import org.junit.Test;
-
 public class IndustryTest {
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link temp.Search#printout(temp.Industry[], java.lang.Double)}.
+	 */
 	@Test
-	public void testSearch() {
+	public void testSearchPrintout() {
 		fail("Not yet implemented");
 	}
 
-	public void testGraphRecommendation() {
+	/**
+	 * Test method for {@link temp.RelaventIndustries#RelaventIndustries()}.
+	 */
+	@Test
+	public void testRIRelaventIndustries() {
 		fail("Not yet implemented");
 	}
 
-	public void testRelaventIndustries() {
+	/**
+	 * Test method for {@link temp.RelaventIndustries#giveRecommendations(java.lang.String[])}.
+	 */
+	@Test
+	public void testRIGiveRecommendations() {
 		fail("Not yet implemented");
 	}
 
-	public void testIndustry() {
+	/**
+	 * Test method for {@link temp.RelaventIndustries#getGraph()}.
+	 */
+	@Test
+	public void testRIGetGraph() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#Name()}.
+	 */
+	@Test
+	public void testIndustryName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#region()}.
+	 */
+	@Test
+	public void testIndustryRegion() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#HourlyEarn()}.
+	 */
+	@Test
+	public void testIndustryHourlyEarn() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#WorkHour()}.
+	 */
+	@Test
+	public void testIndustryWorkHour() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#Time()}.
+	 */
+	@Test
+	public void testIndustryTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#Overtime()}.
+	 */
+	@Test
+	public void testIndustryOvertime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#toString()}.
+	 */
+	@Test
+	public void testIndustryToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link temp.Industry#compareTo(temp.Industry)}.
+	 */
+	@Test
+	public void testIndustryCompareTo() {
 		fail("Not yet implemented");
 	}
 }
